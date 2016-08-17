7d02fe55bc4ab4e5c30a695a606f861a8c4d7ae5
ChemiKyle <kyle.2493@gmail.com>
2016-08-17 00:35:11 -0400

    Update seasonalWall.py

--- seasonalWall.py
+++ seasonalWall.py
@@ -1,7 +1,7 @@
 import os, random, datetime
 
-
-loc = "/home/kyle/Pictures/Wallpapers/branches/"
+# Define the location of files
+loc = "/home/user/path/to/files/"
 
 date = datetime.datetime.now()
 
@@ -10,16 +10,16 @@ dateNum = int(str(date.month) + str(date.day))
 
 
 # Check date against concatenated dates of seasons, rename season accordingly
-if dateNum >= 1221 or dateNum < 320:
+if dateNum >= 1221 or dateNum < 320:	# Winter defined as between 12/21 and 3/20
 	season = "winter/"
-elif dateNum >= 922:
+elif dateNum >= 922:			# Autumn defined as after 9/22
 	season = "autumn/"
-elif dateNum >= 621:
+elif dateNum >= 621:			# Summer defined as after 6/21
 	season = "summer/"
-elif dateNum >= 320:
+elif dateNum >= 320:			# Spring defined as after 3/20
 	season = "spring/"
 
-# Night friendly mode
+# Night friendly mode, with inclusions for seasonality in terms of snow
 if date.hour >= 20 or date.hour <= 6:
 	if dateNum >= 1115 or dateNum <= 405:
 		season = "night/snow/"
@@ -32,6 +32,6 @@ else:
 # pick a random picture
 pic = random.choice(os.listdir(loc + season))
 
-
+# This works for ubuntu, will be testing in debian environments next
 os.system("gsettings set org.gnome.desktop.background picture-uri file://" + loc + season + pic)
 print("Changed to " +  pic)
